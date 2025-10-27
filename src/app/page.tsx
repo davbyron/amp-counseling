5b1a3cf55b39094c61e2745ba54b8eaea5c06c2d
David Byron <davbyron@icloud.com>
2025-10-26 22:33:55 -0400

    place inital layout of page elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,52 @@
-import Image from "next/image";
+import { Baskervville } from "next/font/google";
+import { PiClockFill , PiPhoneFill, PiMapPinFill } from "react-icons/pi";
 
-export default function Home() {
+const baskervville = Baskervville({
+  subsets: ["latin"],
+})
+
+export default async function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="h-full px-5 flex flex-col items-center gap-48">
+      <header className="w-full flex justify-between items-center">
+        <img
+          src="/amp-counseling-logo.svg"
+          alt="AMP Counseling Logo"
+          className="size-30"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+        <address className="not-italic flex flex-col items-end gap-2">
+          <p className="w-full flex items-center justify-between gap-2">
+           <PiPhoneFill />
+           <span>(508) 321-7967</span>
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+          <div className="w-full flex items-center justify-between gap-2">
+            <PiMapPinFill />
+            <p className="flex flex-col items-end">
+              <span>Providence, RI</span>
+              <span>Bridgewater, MA</span>
+            </p>
+          </div>
+        </address>
+      </header>
+      <div className="w-1/2 flex flex-col items-center gap-5 text-center">
+        <h1 className={`${baskervville.className} text-5xl font-medium`}>
+          Aislyn M Plath Counseling 
+        </h1>
+        <h2 className="text-lg">
+          Collaborative and affirming mental health counseling
+        </h2>
+      </div>
+      <footer className="w-full mt-auto mb-5 flex flex-col gap-2">
+        <p className="flex items-center gap-2">
+          <PiClockFill />
+          <span>Monday - Thursday 11am-7pm</span>
+        </p>
+        <img
+          src="/progress-rainbow-flag.svg"
+          alt="Progress rainbow LGBTQ flag"
+          className="w-10"
+        />
+      </footer>
     </div>
   );
 }
