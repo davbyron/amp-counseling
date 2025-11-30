c017d768d43deeac7a37d0ea68a0e9f5d63e2940
David Byron <davbyron@icloud.com>
2025-11-30 16:41:49 -0500

    create types folder and props file

--- src/components/GlowingBall.tsx
+++ src/components/GlowingBall.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-
-interface GlowingBallProps {
-  size: string | number;
-  right: string | number;
-  top: string | number;
-  shadowSize: "sm" | "md" | "xl";
-}
+import { GlowingBallProps } from "@/types";
 
 export function GlowingBall(props: GlowingBallProps) {
   const {
---
+++ src/types/props.ts
@@ -0,0 +1,6 @@
+export interface GlowingBallProps {
+  size: string | number;
+  right: string | number;
+  top: string | number;
+  shadowSize: "sm" | "md" | "xl";
+}
